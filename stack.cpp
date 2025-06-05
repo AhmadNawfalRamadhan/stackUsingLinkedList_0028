3bf866df147bf7ced07885b096fe9fb3aa3f1180
AhmadNawfalRamadhan <Ahmadnawfalramadhan@gmail.com>
2025-06-05 11:03:25 +0700

    membuat 4 pilihan di int main

--- stack.cpp
+++ stack.cpp
@@ -85,5 +85,10 @@ int value;
 while (choice != 4)
 {
     cout << "1. Push\n";
+    cout << "2. Pop\n";
+    cout << "3. Peek\n";
+    cout << "4. Exit\n";
+    cout << "Enter your choice: ";
+    cin >> choice;
 }
 }
