eb72d68888a8c7299af2c63ee4f18f65e2eeec3b
AhmadNawfalRamadhan <Ahmadnawfalramadhan@gmail.com>
2025-06-05 10:40:08 +0700

    class stack and new node Part 1

--- stack.cpp
+++ stack.cpp
@@ -13,3 +13,28 @@ class Node
         next = NULL;
     }
 };
+
+//stack class
+class Stack
+{
+    private:
+    Node *top; // pointer to the top node of the stack
+
+    public:
+    Stack()
+    {
+        top = NULL; // initialize the stack with a null top pointer
+    }
+
+    //push operation insert an element onto the top of the stack
+    int push(int value)
+    {
+        Node *newNode = new Node(); //Allocate memory for the mew node
+        newNode->data = value; // assign value
+        newNode->next = top; // set the next pointer of the new node to the current top
+        top = newNode; // update the top pointer to the new node
+        cout << "push value: " << value << endl;
+        return value;
+    }
+
+}
